5c32354b4dece9d681cf495e94f6cfafd82d0003
kay <kayshiro@live.no>
2022-01-05 20:39:27 +0100

    Fragment set up done

--- app/src/main/java/com/kay/volumebarsdemo/fragment/VolumeFragment.kt
+++ app/src/main/java/com/kay/volumebarsdemo/fragment/VolumeFragment.kt
@@ -1,20 +1,19 @@
 package com.kay.volumebarsdemo.fragment
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import com.kay.volumebarsdemo.R
 
 class VolumeFragment : Fragment() {
-
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_volume, container, false)
     }
-
 }
